8f33860ef7a916b609bb0d813173d007d757f3ad
YATACHI <60470788+p2510@users.noreply.github.com>
2023-01-31 13:53:30 +0100

    Update header.blade.php

--- resources/views/layouts/includes/header.blade.php
+++ resources/views/layouts/includes/header.blade.php
@@ -98,7 +98,7 @@
                             <div class="widget">
                                 <ul class="list-inline text-right flip sm-text-center">
                                     <li>
-                                        <a class="text-white" href="#">FAQ</a>
+                                        <a class="text-white" href="#">FAQs</a>
                                     </li>
                                     <li class="text-white">|</li>
                                     <li>
