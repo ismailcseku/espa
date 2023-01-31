2f91ad6f20e3813ffa4fcc0204c800bf0e88b1c7
YATACHI <60470788+p2510@users.noreply.github.com>
2023-01-31 23:19:32 +0100

    Update header.blade.php

--- resources/views/layouts/includes/header.blade.php
+++ resources/views/layouts/includes/header.blade.php
@@ -18,7 +18,7 @@
     <link href="images/apple-touch-icon-72x72.png" rel="apple-touch-icon" sizes="72x72">
     <link href="images/apple-touch-icon-114x114.png" rel="apple-touch-icon" sizes="114x114">
     <link href="images/apple-touch-icon-144x144.png" rel="apple-touch-icon" sizes="144x144">
-
+ 
     <!-- Stylesheet -->
     <link href="{{ asset('app-assets/css/bootstrap.min.css') }}" rel='stylesheet' type='text/css'>
     <link href="{{ asset('app-assets/css/jquery-ui.min.css') }}" rel="stylesheet" type="text/css">
