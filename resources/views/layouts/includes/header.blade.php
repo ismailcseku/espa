702b718ab8fa6f6fb3ae68922acf2a008aba6922
YATACHI <60470788+p2510@users.noreply.github.com>
2023-01-31 13:51:47 +0100

    preloader

--- resources/views/layouts/includes/header.blade.php
+++ resources/views/layouts/includes/header.blade.php
@@ -74,7 +74,7 @@
             <div id="spinner">
                 <img alt="" src="images/preloaders/5.gif">
             </div>
-            <div id="disable-preloader" class="btn btn-default btn-sm">Disable Preloader</div>
+            <div id="disable-preloader" class="btn btn-default btn-sm">Chargement</div>
         </div>
 
         <!-- Header -->
