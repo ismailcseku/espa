ed34c93953a720b5de49a0d4592d8074cf561683
YATACHI <60470788+p2510@users.noreply.github.com>
2023-02-01 03:28:30 +0100

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,193 @@
 @extends('layouts.app')
 @section('content')
     <div class="main-content">
-     
+        <!-- Section: home -->
+        <section id="home" class="divider">
+            <!-- START REVOLUTION SLIDER 5.0.7 -->
+            <div id="rev_slider_home_wrapper" class="rev_slider_wrapper fullwidthbanner-container" data-alias="news-gallery34"
+                style="margin:0px auto;background-color:#ffffff;padding:0px;margin-top:0px;margin-bottom:0px;">
+                <!-- START REVOLUTION SLIDER 5.0.7 fullwidth mode -->
+                <div id="rev_slider_home" class="rev_slider fullwidthabanner" style="display:none;" data-version="5.0.7">
+                    <ul>
+                        <!-- SLIDE 1 -->
+                        @foreach ($slides as $slide)
+                            <li>
+                                <!-- MAIN IMAGE -->
+                                <img src="{{ url('storage')}}/{{ $slide->photo }}" alt=""
+                                    data-bgposition="center 20%" data-bgfit="cover" data-bgrepeat="no-repeat"
+                                    data-bgparallax="10" class="rev-slidebg" data-no-retina>
+                                <!-- LAYERS -->
+                                <!-- LAYER NR. 1 -->
+                                <div class="tp-caption tp-resizeme text-white rs-parallaxlevel-0" id="slide-1-layer-1"
+                                    data-x="['left','left','left','left']" data-hoffset="['50','50','50','30']"
+                                    data-y="['top','top','top','top']" data-voffset="['215','130','110','120']"
+                                    data-fontsize="['20','18','16','13']" data-lineheight="['30','30','28','25']"
+                                    data-fontweight="['700','700','700','700']" data-width="['700','650','600','420']"
+                                    data-height="none" data-whitespace="nowrap" data-transform_idle="o:1;"
+                                    data-transform_in="y:[-100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;s:1500;e:Power3.easeInOut;"
+                                    data-transform_out="auto:auto;s:1000;e:Power3.easeInOut;"
+                                    data-mask_in="x:0px;y:0px;s:inherit;e:inherit;"
+                                    data-mask_out="x:0;y:0;s:inherit;e:inherit;" data-start="1000" data-splitin="none"
+                                    data-splitout="none" data-responsive_offset="on"
+                                    style="z-index: 7; white-space: nowrap;">{{ $slide->subject }}
+                                </div>
+                                <!-- LAYER NR. 2 -->
+                                <div class="tp-caption tp-resizeme text-white text-uppercase font-montserrat rs-parallaxlevel-0"
+                                    id="slide-1-layer-2" data-x="['left','left','left','left']"
+                                    data-hoffset="['50','50','50','30']" data-y="['top','top','top','top']"
+                                    data-voffset="['250','160','140','150']" data-fontsize="['52','46','40','28']"
+                                    data-lineheight="['68','60','54','42']" data-fontweight="['800','800','800','800']"
+                                    data-width="['700','650','600','420']" data-height="none" data-whitespace="normal"
+                                    data-transform_idle="o:1;"
+                                    data-transform_in="y:[-100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;s:1500;e:Power3.easeInOut;"
+                                    data-transform_out="auto:auto;s:1000;e:Power3.easeInOut;"
+                                    data-mask_in="x:0px;y:0px;s:inherit;e:inherit;"
+                                    data-mask_out="x:0;y:0;s:inherit;e:inherit;" data-start="1000" data-splitin="none"
+                                    data-splitout="none" data-responsive_offset="on"
+                                    style="z-index: 6; min-width: 600px; max-width: 600px; white-space: normal;">
+                                    {{ $slide->title }}
+                                </div>
+                                <!-- LAYER NR. 3 -->
+                                <div class="tp-caption tp-resizeme text-white rs-parallaxlevel-0" id="slide-1-layer-3"
+                                    data-x="['left','left','left','left']" data-hoffset="['50','50','50','30']"
+                                    data-y="['top','top','top','top']" data-voffset="['325','220','195','195']"
+                                    data-fontsize="['16','16','14','13']" data-lineheight="['30','26','24','20']"
+                                    data-fontweight="['400','400','400','400']" data-width="['700','650','600','420']"
+                                    data-height="none" data-whitespace="nowrap" data-transform_idle="o:1;"
+                                    data-transform_in="y:[-100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;s:1500;e:Power3.easeInOut;"
+                                    data-transform_out="auto:auto;s:1000;e:Power3.easeInOut;"
+                                    data-mask_in="x:0px;y:0px;s:inherit;e:inherit;"
+                                    data-mask_out="x:0;y:0;s:inherit;e:inherit;" data-start="1000" data-splitin="none"
+                                    data-splitout="none" data-responsive_offset="on"
+                                    style="z-index: 7; white-space: nowrap;">
+                                    @foreach ($slide->description as $item)
+                                        {{ $item }} <br>
+                                    @endforeach
+                                </div>
+                                <!-- LAYER NR. 4 -->
+                                <div class="tp-caption tp-resizeme text-white rs-parallaxlevel-0" id="slide-1-layer-4"
+                                    data-x="['left','left','left','left']" data-hoffset="['53','53','53','30']"
+                                    data-y="['top','top','top','top']" data-voffset="['410','290','260','250']"
+                                    data-fontsize="['18','18','16','16']" data-lineheight="['30','30','30','30']"
+                                    data-fontweight="['600','600','600','600']" data-width="['700','650','600','420']"
+                                    data-height="none" data-whitespace="nowrap" data-transform_idle="o:1;"
+                                    data-transform_in="y:[-100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;s:1500;e:Power3.easeInOut;"
+                                    data-transform_out="auto:auto;s:1000;e:Power3.easeInOut;"
+                                    data-mask_in="x:0px;y:0px;s:inherit;e:inherit;"
+                                    data-mask_out="x:0;y:0;s:inherit;e:inherit;" data-start="1000" data-splitin="none"
+                                    data-splitout="none" data-responsive_offset="on"
+                                    style="z-index: 7; white-space: nowrap;"><a href="{{route('course.index')}}"
+                                        class="btn btn-dark btn-circled btn-theme-colored2 btn-xl mr-10 pr-30 pl-30">Découvrir</a>
+                                    <a href="{{route('contact.create')}}"
+                                        class="btn btn-transparent btn-border btn-circled btn-xl pr-30 pl-30">Contact</a>
+                                </div>
+                            </li>
+                        @endforeach
+                       
+
+                    </ul>
+                    <div class="tp-bannertimer tp-bottom"
+                        style="height: 5px; background-color: rgba(255, 255, 255, 0.4);"></div>
+                </div>
+            </div>
+            <!-- END REVOLUTION SLIDER -->
+            <script>
+                var tpj = jQuery;
+                var revapi34;
+                tpj(document).ready(function() {
+                    if (tpj("#rev_slider_home").revolution == undefined) {
+                        revslider_showDoubleJqueryError("#rev_slider_home");
+                    } else {
+                        revapi34 = tpj("#rev_slider_home").show().revolution({
+                            sliderType: "standard",
+                            jsFileLocation: "js/revolution-slider/js/",
+                            sliderLayout: "fullwidth",
+                            dottedOverlay: "none",
+                            delay: 9000,
+                            navigation: {
+                                keyboardNavigation: "on",
+                                keyboard_direction: "horizontal",
+                                mouseScrollNavigation: "off",
+                                onHoverStop: "on",
+                                touch: {
+                                    touchenabled: "on",
+                                    swipe_threshold: 75,
+                                    swipe_min_touches: 1,
+                                    swipe_direction: "horizontal",
+                                    drag_block_vertical: false
+                                },
+                                arrows: {
+                                    enable: true,
+                                    style: 'gyges',
+                                    left: {
+                                        h_align: "left",
+                                        v_align: "center",
+                                        h_offset: 0,
+                                        v_offset: 0
+                                    },
+                                    right: {
+                                        h_align: "right",
+                                        v_align: "center",
+                                        h_offset: 0,
+                                        v_offset: 0
+                                    }
+                                },
+                                bullets: {
+                                    enable: true,
+                                    style: 'hebe',
+                                    tmp: '<span class="tp-bullet-image"></span>',
+                                    hide_onmobile: true,
+                                    hide_under: 600,
+                                    hide_onleave: true,
+                                    hide_delay: 200,
+                                    hide_delay_mobile: 1200,
+                                    direction: "horizontal",
+                                    h_align: "center",
+                                    v_align: "bottom",
+                                    h_offset: 0,
+                                    v_offset: 30,
+                                    space: 5
+                                }
+                            },
+                            viewPort: {
+                                enable: true,
+                                outof: "pause",
+                                visible_area: "80%"
+                            },
+                            responsiveLevels: [1240, 1024, 778, 480],
+                            gridwidth: [1240, 1024, 778, 480],
+                            gridheight: [660, 550, 500, 450],
+                            lazyType: "none",
+                            parallax: {
+                                type: "scroll",
+                                origo: "enterpoint",
+                                speed: 400,
+                                levels: [5, 10, 15, 20, 25, 30, 35, 40, 45, 50],
+                            },
+                            shadow: 0,
+                            spinner: "off",
+                            stopLoop: "off",
+                            stopAfterLoops: -1,
+                            stopAtSlide: 1,
+                            shuffle: "off",
+                            autoHeight: "off",
+                            hideThumbsOnMobile: "off",
+                            hideSliderAtLimit: 0,
+                            hideCaptionAtLimit: 0,
+                            hideAllCaptionAtLilmit: 0,
+                            debugMode: false,
+                            fallbacks: {
+                                simplifyAll: "off",
+                                nextSlideOnWindowFocus: "off",
+                                disableFocusListener: false,
+                            }
+                        });
+                    }
+                }); /*ready*/
+            </script>
+            <!-- END REVOLUTION SLIDER -->
+    </div>
+    </section>
 
     <!-- Section Features -->
     <section id="features" class="bg-silver-deep">
