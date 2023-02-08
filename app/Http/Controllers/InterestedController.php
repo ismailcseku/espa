d262aeae12889c0573a6451a495be2c534a57433
YATACHI <60470788+p2510@users.noreply.github.com>
2023-02-08 16:04:03 +0100

    update

--- app/Http/Controllers/InterestedController.php
+++ app/Http/Controllers/InterestedController.php
@@ -20,6 +20,7 @@ class InterestedController extends Controller
      */
     public function __invoke(Request $request ,int $id)
     {
+    dd('dffdgh');
    $data=$request->validate([
             'gender'=>['required','string',Rule::in(['Mlle','Mme','Mr'])],
             'name'=>['required','string','max:50','min:2'],
@@ -33,7 +34,7 @@ class InterestedController extends Controller
            'grade'=>['required','string'],
            'accepted'=>['required','string'],
         ]);  
-        dd($data);
+        
         $data['course_id']=$id;
         
         Interested::create($data);
