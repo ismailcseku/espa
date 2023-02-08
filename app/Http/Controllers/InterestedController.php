42c52414d81cf2957a6fe33edec9038d4a6d5d0b
YATACHI <60470788+p2510@users.noreply.github.com>
2023-02-08 16:15:15 +0100

    debug 6

--- app/Http/Controllers/InterestedController.php
+++ app/Http/Controllers/InterestedController.php
@@ -26,11 +26,7 @@ class InterestedController extends Controller
             'name'=>['required','string','max:50','min:2'],
             'surname'=>['required','string','max:100','min:2'],
             'email'=>['required','email'],
-            'birth'=>['required','date','before:tomorrow'],
             
-            'phone'=>['required','string'],
-            'level'=>['required','string',Rule::in(Levels::getting())],
-           'grade'=>['required','string'],
            'accepted'=>['required','string'],
         ]);  
         dd($data);
