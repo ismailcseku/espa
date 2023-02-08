5817b13b4dbaa13196703caa0e1a65e96512cb2a
YATACHI <60470788+p2510@users.noreply.github.com>
2023-02-08 16:07:39 +0100

    debug #1

--- app/Http/Controllers/InterestedController.php
+++ app/Http/Controllers/InterestedController.php
@@ -23,16 +23,7 @@ class InterestedController extends Controller
     
    $data=$request->validate([
             'gender'=>['required','string',Rule::in(['Mlle','Mme','Mr'])],
-            'name'=>['required','string','max:50','min:2'],
-             'surname'=>['required','string','max:100','min:2'],
-            'email'=>['required','email'],
-            'birth'=>['required','date','before:tomorrow'],
-            'country'=>['required','string',Rule::in(Countries::getting())],
-            'province'=>['string'],
-            'phone'=>['required','string'],
-            'level'=>['required','string',Rule::in(Levels::getting())],
-           'grade'=>['required','string'],
-           'accepted'=>['required','string'],
+         
         ]);  
         dd($data);
         
