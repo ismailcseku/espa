4213ac29f61c115386f0a0c1548536416daf386e
YATACHI <60470788+p2510@users.noreply.github.com>
2023-03-09 12:09:02 +0100

    update

--- database/migrations/2023_01_30_211003_create_interesteds_table.php
+++ database/migrations/2023_01_30_211003_create_interesteds_table.php
@@ -25,7 +25,7 @@ return new class extends Migration
             $table->string('phone');
             $table->string('grade')->default('');
             $table->string('accepted');
-            /*$table->foreignId("course_id");
+            /*$table->foreignId("course_id"); 
             $table->foreign('course_id')->references('id')->on('courses')->onDelete('cascade');*/
             $table->timestamps();
         });
