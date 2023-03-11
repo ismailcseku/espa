a4e6591f8a3037f7bf050f32a6851211634f18bd
YATACHI <60470788+p2510@users.noreply.github.com>
2023-03-11 14:41:14 +0100

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -512,41 +512,7 @@
         </div>
     </section>
 
-    <section style="background: linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(31,51,68,1) 58%, rgba(40,40,46,1) 100%);" >
-        <div class="container pt-80 pb-90">
-            <div class="row">
-                <div class="col-md-8 col-md-offset-2 text-center">
-                    <a href="https://www.youtube.com/watch?v=kt-4lJs_8fE" data-lightbox-gallery="youtube-video"><img
-                            src="images/play-button/s11.png" alt=""></a>
-                    <h3 class="text-white font-38 font-weight-700 mt-10 mb-0">Inscrivez-vous <span
-                            class="text-theme-colored2"> à notre newsletter</span> </h3>
-                    Rejoignez-nous
-                    @if (session()->has('success'))
-                        <h6 class='alert alert-success' role="alert">
-                            Felicitation !! Vous avez êtes désormais abonnée.
-                        </h6>
-                    @endif
-                    <form  id="mailchimp-subscription-form2" class="newsletter-form max-width-535 mt-10"
-                        action="{{ route('newsletter.store') }}" method="post">
-                        @csrf
-                        <label for="mce-EMAIL"></label>
-                        <div class="input-group">
-                            <input type="email" id="mce-EMAIL" data-height="45px" class="form-control input-lg"
-                                placeholder="Votre email" name="email" value="{{ old('email') }}">
-                            @if ($errors->has('email'))
-                                <span class="text-danger">{{ $errors->first('email') }}</span>
-                            @endif
-                            <span class="input-group-btn">
-                                <button type="submit" class="btn btn-colored btn-theme-colored2 btn-lg m-0"
-                                    data-height="45px">Soumettre</button>
-                            </span>
-                        </div>
-                    </form>
 
-                </div>
-            </div>
-        </div>
-    </section>
 
 
     <section class="clients ">
