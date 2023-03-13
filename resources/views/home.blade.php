94380a4f723aa6eeaa5514aecb38709683a62b35
YATACHI <60470788+p2510@users.noreply.github.com>
2023-03-13 13:43:53 +0100

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -145,7 +145,7 @@
         @media only screen and (min-width: 600px) {
             .slide div {
                 top: 45%;
-                width: 30%;
+                width: 36%;
                 text-align: justify;
             }
 
